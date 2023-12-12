8b81cf656c4c8d602c3e435ee364ca63bbb51d9c
CyanFaux <147452367+CyanFaux@users.noreply.github.com>
2023-12-12 18:50:19 -0500

    Add files via upload

---
+++ New folder (2)/logic.js
@@ -0,0 +1,92 @@
+let header = document.createElement('h1');
+header.textContent = "Warranty Information"
+
+let firstName = document.createElement('label');
+firstName.textContent = '';
+
+let firstName = document.createElement('input');
+firstName.setAttribute('text')
+
+let lastName = document.createElement('label');
+lastName.textContent = '';
+
+let lastName = document.createElement('input');
+lastName.setAttribute('text')
+
+let companyName = document.createElement('label');
+.textContent = '';
+
+let companyName = document.createElement('input');
+.setAttribute('text')
+
+let email = document.createElement('label');
+.textContent = '';
+
+let email = document.createElement('input');
+.setAttribute('text')
+
+let phoneNumber = document.createElement('label');
+.textContent = '';
+
+let phoneNumber = document.createElement('input');
+.setAttribute('text')
+
+let faxNumber = document.createElement('label');
+.textContent = '';
+
+let faxNumber = document.createElement('input');
+.setAttribute('text')
+
+let address = document.createElement('label');
+.textContent = '';
+
+let address = document.createElement('input');
+.setAttribute('text')
+
+let city = document.createElement('label');
+.textContent = '';
+
+let city = document.createElement('input');
+.setAttribute('text')
+
+let state = document.createElement('label');
+.textContent = '';
+
+let state = document.createElement('input');
+.setAttribute('text')
+
+let zip = document.createElement('label');
+.textContent = '';
+
+let zip = document.createElement('input');
+.setAttribute('text')
+
+let country = document.createElement('label');
+.textContent = '';
+
+let country = document.createElement('input');
+.setAttribute('text')
+
+let unitInformation = document.createElement('label');
+.textContent = '';
+
+let unitInformation = document.createElement('input');
+.setAttribute('text')
+
+let dateOfPurchase = document.createElement('label');
+.textContent = '';
+
+let dateOfPurchase = document.createElement('input');
+.setAttribute('text')
+
+let model = document.createElement('label');
+.textContent = '';
+
+let model = document.createElement('input');
+.setAttribute('text')
+
+let warrantySerialNumber = document.createElement('label');
+.textContent = '';
+
+let warrantySerialNumber = document.createElement('input');
+.setAttribute('text')
